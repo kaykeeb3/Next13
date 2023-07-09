1866fe95a054b5ecc42ecc9c528fef5c876346bc
Kayke Barbosa <104206837+Kayke-Ti@users.noreply.github.com>
2023-07-09 09:22:51 -0300

    add new commit page

--- src/app/dashbaord/page.tsx
+++ src/app/dashbaord/page.tsx
@@ -4,7 +4,7 @@ export default function Dashbaord() {
   return (
     <div className="bg-zinc-800 w-full h-screen">
       <h1 className="text-center font-medium text-2xl text-white pt-3">
-        Dashboard
+        Dashboard:
       </h1>
 
       <div className="bg-zinc-800  flex justify-center mt-7 items-center">
@@ -12,7 +12,7 @@ export default function Dashbaord() {
           href="/"
           className="bg-green-500 font-sans text-center w-48 rounded-lg h-16 flex items-center justify-center"
         >
-          Home
+          Home:
         </Link>
       </div>
     </div>
